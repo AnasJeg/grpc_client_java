1fb56c80296298f1a98a88fc81172f608c30fdb8
AnasJeg <anasjegoual.2000@gmail.com>
2023-10-31 23:31:37 +0100

    FC Client

--- src/main/java/org/example/client/GrpcClient.java
+++ src/main/java/org/example/client/GrpcClient.java
@@ -1,2 +1,24 @@
-package org.example.client;public class GrpcClient {
+package org.example.client;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.example.grpc_stubs.GrpcServerGrpc;
+import org.example.grpc_stubs.GrpcServerOuterClass;
+
+public class GrpcClient {
+    public static void main(String[] args) {
+        // sync
+        ManagedChannel managedChannel= ManagedChannelBuilder.forAddress("localhost",5050)
+                .usePlaintext()
+                .build();
+        GrpcServerGrpc.GrpcServerBlockingStub grpcServerBlockingStub=GrpcServerGrpc.newBlockingStub(managedChannel);
+        GrpcServerOuterClass.DashbordRequest request= GrpcServerOuterClass.DashbordRequest.newBuilder()
+                        .setRef("test")
+                                .setMarque("hp")
+                                        .setPrix(1234.66)
+                                                .build();
+       GrpcServerOuterClass.DashbordResponse response= grpcServerBlockingStub.dashbordMachines(request);
+        System.out.println(response);
+
+    }
 }
